b15febce377559e0954da1134cb2d33f88d3a430
caizj <caizj@ipampas.com>
2019-09-03 15:32:18 +0800

    打包报错

--- src/main/java/com/company/project/core/AbstractIpampasController.java
+++ src/main/java/com/company/project/core/AbstractIpampasController.java
@@ -1,12 +1,6 @@
 package com.company.project.core;
 
 
-import com.ipampas.panshi.system.dto.UserDto;
-import com.ipampas.panshi.system.service.UserService;
-import com.ipampas.panshi.ucenter.user.TenantUserContext;
-
-import javax.annotation.Resource;
-
 /**
  * @author caizj
  * @description: Controller层公共方法
@@ -14,19 +8,6 @@ import javax.annotation.Resource;
  * @since 1.0.0
  */
 public abstract class AbstractIpampasController {
-
-    @Resource
-    private UserService userService;
-
-    /**
-     * 根据guid获取机构用户
-     *
-     * @return
-     */
-    protected UserDto getUserByGuid() {
-        //
-        return userService.findByGuid(TenantUserContext.get().getUserGuid());
-    }
 
 
 }
